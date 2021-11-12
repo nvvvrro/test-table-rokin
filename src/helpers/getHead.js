c6c0ec508204f5bf8242d17c8f2e1b750d5155f4
Marcelo Navarro <84986697+nvvvrro@users.noreply.github.com>
2021-11-12 12:48:24 -0300

    add data json

---
+++ src/helpers/getHead.js
@@ -0,0 +1,46 @@
+const headCells = [
+  {
+    id: "rut",
+    numeric: true,
+    disablePadding: true,
+    label: "Rut",
+  },
+  {
+    id: "nombre",
+    numeric: true,
+    disablePadding: false,
+    label: "Nombre",
+  },
+  {
+    id: "correo",
+    numeric: true,
+    disablePadding: false,
+    label: "Correo",
+  },
+  {
+    id: "telefono",
+    numeric: true,
+    disablePadding: false,
+    label: "Telefono",
+  },
+  {
+    id: "rol",
+    numeric: true,
+    disablePadding: false,
+    label: "Rol",
+  },
+  {
+    id: "empresa",
+    numeric: true,
+    disablePadding: false,
+    label: "Empresa",
+  },
+  {
+    id: "creacion",
+    numeric: true,
+    disablePadding: false,
+    label: "Creacion",
+  },
+];
+
+export default headCells;
